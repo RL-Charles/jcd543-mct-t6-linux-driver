57bb8d23c83207c8dab398879e674d6a5ff43eac
Alex Friedman <alexfriedman@asus-zenbook.tailace7b.ts.net>
2026-04-06 10:59:34 -0400

    jpeg: realign native color conversion with JFIF path

--- kernel/trigger6_jpeg.c
+++ kernel/trigger6_jpeg.c
@@ -16,8 +16,7 @@
 #include "trigger6_jpeg.h"
 
 #define T6_JPEG_COMPONENTS	3
-/* DCT matrix constants are scaled to ~8192 units. */
-#define T6_JPEG_DCT_SCALE	8192
+#define T6_JPEG_DCT_SCALE	16384
 #define T6_JPEG_MAX_HUFF_BITS	16
 
 struct t6_jpeg_huff_table {
@@ -434,8 +433,8 @@ static void t6_jpeg_load_y_block(const u8 *src,
 			int red = pixel[2];
 			int y_value;
 
-			/* BT.601 studio-range luma (16..235) for T6 decoder parity. */
-			y_value = ((66 * red + 129 * green + 25 * blue + 128) >> 8) + 16;
+			/* Match the working Pillow/JFIF full-range RGB->YCbCr conversion. */
+			y_value = (77 * red + 150 * green + 29 * blue + 128) >> 8;
 			block[y * 8 + x] = t6_jpeg_clamp_byte(y_value) - 128;
 		}
 	}
@@ -477,9 +476,9 @@ static void t6_jpeg_load_chroma_block(const u8 *src,
 			blue = DIV_ROUND_CLOSEST(blue, 4);
 
 			cb_block[y * 8 + x] =
-				t6_jpeg_clamp_byte(((-38 * red - 74 * green + 112 * blue + 128) >> 8) + 128) - 128;
+				t6_jpeg_clamp_byte(((-43 * red - 85 * green + 128 * blue + 128) >> 8) + 128) - 128;
 			cr_block[y * 8 + x] =
-				t6_jpeg_clamp_byte(((112 * red - 94 * green - 18 * blue + 128) >> 8) + 128) - 128;
+				t6_jpeg_clamp_byte(((128 * red - 107 * green - 21 * blue + 128) >> 8) + 128) - 128;
 		}
 	}
 }
